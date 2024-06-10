17cd03675223b82e95a3cc5c6786d71c72b42eee
Cristobal Amaya <g3nericn4med@gmail.com>
2024-06-09 22:45:44 -0400

    cambios

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { PageComponent } from './Ingredientes/components/page/page.component';
+import { LoginComponent } from './Users/components/login/login.component';
 
 export const routes: Routes = [
     { path: '', component: AppComponent },
-    { path: 'ingredientes', component: PageComponent } 
+    { path: 'ingredientes', component: PageComponent },
+    { path: 'login', component: LoginComponent },
 ];
