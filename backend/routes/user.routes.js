eaf8754d659284ab0f56e14c835d432c771ae16a
EstebanA1 <esteban.rivas2001@alumnos.ubiobio.cl>
2024-07-26 00:17:39 -0400

    Se agrego mensaje de error con users

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,16 +1,7 @@
 "use strict";
 const express = require("express");
 const usuarioController = require("../controllers/user.controller.js");
-const authorizationMiddleware = require("../middlewares/authorization.middleware.js");
-const authenticationMiddleware = require("../middlewares/authentication.middleware.js");
 const router = express.Router();
-
-// router.put("/:id", authenticationMiddleware, authorizationMiddleware.isAdmin, usuarioController.updateUser);
-// router.delete("/:id", authenticationMiddleware, authorizationMiddleware.isAdmin, usuarioController.deleteUser);
-
-// router.post("/register", authenticationMiddleware, authorizationMiddleware.isAdmin, usuarioController.createUser);
-// router.get("/", authenticationMiddleware, authorizationMiddleware.isAdmin, usuarioController.getUsers);
-// router.get("/:id", authenticationMiddleware, authorizationMiddleware.isAdmin, usuarioController.getUserById);
 
 router.put("/:id", usuarioController.updateUser);
 router.delete("/:id", usuarioController.deleteUser);
@@ -20,10 +11,10 @@ router.get("/", usuarioController.getUsers);
 router.get("/:id", usuarioController.getUserById);
 
 // Muestran mensajes cuando se necesita enviar ID pero esta vacio
-// router.delete('/', authenticationMiddleware, authorizationMiddleware.isAdmin, usuarioController.handleMissingId);
-// router.put('/', authenticationMiddleware, authorizationMiddleware.isAdmin, usuarioController.handleMissingId);
+router.delete('/', usuarioController.handleMissingId);
+router.put('/', usuarioController.handleMissingId);
 
 // Muestra mensaje cuando no haya que usar un ID pero se haya enviado
-// router.post('/:id', authenticationMiddleware, authorizationMiddleware.isAdmin, usuarioController.handleId);
+router.post('/:id', usuarioController.handleId);
 
 module.exports = router;
