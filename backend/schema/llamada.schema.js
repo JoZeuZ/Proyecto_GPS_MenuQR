0e0d32074398d077ebb4c3257b73827d0f57b8f7
Lucas Hernández <lucas.hernandez2001@alumnos.ubiobio.cl>
2024-07-26 20:29:37 -0400

    Se elimina nombre en llamada se deja solo la mesa

--- backend/schema/llamada.schema.js
+++ backend/schema/llamada.schema.js
@@ -8,11 +8,6 @@ const callWaiterSchema = Joi.object({
         "number.base": "El número de mesa debe ser un número.",
         "any.required": "El número de mesa es obligatorio.",
     }),
-    customerName: Joi.string().required().messages({
-        "string.empty": "El nombre del cliente no puede estar vacío.",
-        "any.required": "El nombre del cliente es obligatorio.",
-        "string.base": "El nombre del cliente debe ser de tipo string.",
-    }),
 }).messages({
     "object.unknown": "No se permiten propiedades adicionales.",
 });
